00a9a76c19daca225048fabe4496cb73d11dbf4f
Jomo Fisher <jomof@google.com>
2019-11-03 11:27:53 -0800

    Set up build

--- upload-gs/upload-gs.sh
+++ upload-gs/upload-gs.sh
@@ -4,6 +4,6 @@ echo UPLOAD_FOLDER=[${UPLOAD_FOLDER}]
 echo SUBPROJECT_ID=[${SUBPROJECT_ID}]
 echo BUILD_ID=[${BUILD_ID}]
 echo Upload $UPLOAD_FOLDER to gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID/latest/
-gsutil cp $UPLOAD_FOLDER/./* gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID/latest/
-gsutil cp $UPLOAD_FOLDER/./* gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID/commit-$SHORT_SHA/
-gsutil cp $UPLOAD_FOLDER/./* gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID/build-$BUILD_ID/
+gsutil cp -r $UPLOAD_FOLDER/./* gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID/latest/
+gsutil cp -r $UPLOAD_FOLDER/./* gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID/commit-$SHORT_SHA/
+gsutil cp -r $UPLOAD_FOLDER/./* gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID/build-$BUILD_ID/
