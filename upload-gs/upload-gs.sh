0ff8d5cea97f0dc8f51ed7f82b74cf761419d555
Jomo Fisher <jomof@google.com>
2019-11-03 13:01:01 -0800

    Set up build

--- upload-gs/upload-gs.sh
+++ upload-gs/upload-gs.sh
@@ -1,9 +1,6 @@
 UPLOAD_FOLDER=${UPLOAD_FOLDER:-.}
 SHORT_SHA=${SHORT_SHA:-local-build}
 PACKAGE_VERSION=${PACKAGE_VERSION:-0.0.1}
-echo UPLOAD_FOLDER=[${UPLOAD_FOLDER}]
-echo SUBPROJECT_ID=[${SUBPROJECT_ID}]
-echo BUILD_ID=[${BUILD_ID}]
 echo gsutil rsync $UPLOAD_FOLDER gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID-${PACKAGE_VERSION}/latest/
 gsutil rsync $UPLOAD_FOLDER gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID-${PACKAGE_VERSION/latest/
 gsutil rsync $UPLOAD_FOLDER gs://my-web-apis_cloudbuild/artifacts/$SUBPROJECT_ID-${PACKAGE_VERSION/commit-$SHORT_SHA/
